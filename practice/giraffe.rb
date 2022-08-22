96662be5f025e767b9b3e774a46f3245a45fd700
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-22 12:27:42 -0500

    still learning ruby;

--- practice/giraffe.rb
+++ practice/giraffe.rb
@@ -1,6 +1,77 @@
+# age = 100
+# name = "Philly"
 
-puts "    /|"
-puts "   / |"
-puts "  /  |"
-puts " /   |"
-puts "/____|"
+# puts name, age
+
+
+# puts "    /|"
+# puts "   / |"
+# puts "  /  |"
+# puts " /   |"
+# puts "/____|"
+
+
+# type_string = "     String \n nice \'supposedly\'        "
+# type_number = 12.5
+# type_boolean = true
+# type_nil = nil 
+
+# puts type_string.upcase()
+# puts type_string.strip()
+# puts (type_string.strip()).length()
+# puts type_string.include? "nice" 
+# puts (type_string.strip())[0, 10]
+# puts type_string.index("i")
+# puts "i'm not shouting, you are".upcase()
+
+# puts 5
+# puts -1238239487203
+# puts 3 * 29
+# puts 2**3
+# puts 10 % 3
+
+# puts -10.abs()
+# puts 342.234235.round()
+# puts 342.234235.floor()
+# puts 342.234235.ceil()
+# puts (Math.sqrt(25))
+# puts 10 / 7.0
+# puts 10 / 7
+
+
+# target full bernefits for both
+# walmart
+# target starts 9 26
+# walmart starts 10 18
+# capella university
+# tied to the duration of the cohort
+# japoaneese garden portland japaneese garden
+# 62,400 salary
+# 2pm central 30 min call
+# bring questions 
+
+
+# puts "Enter your name:"
+# name = gets.chomp()
+# puts "Enter your age:"
+# age = gets.chomp()
+# puts ("Hello " + name + ", you are " + age)
+
+puts "Give a number"
+num_1 = gets.chomp()
+puts "Give a second number"
+num_2 = gets.chomp()
+puts (num_1.to_f + num_2.to_f)
+# to_i is to integer 
+# to_f is floating point
+
+puts "Give a noun"
+noun = gets
+puts "Give a number"
+number = gets
+puts "Give a verb"
+verb = gets
+puts "Give a adjective"
+adjective = gets
+
+puts "{noun} {verb} for {number} {adjective} cats "
